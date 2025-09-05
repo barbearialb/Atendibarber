91b4855a54df47816d866795d83254836b143b81
barbearialb <barbearialucasborges@gmail.com>
2025-09-04 23:25:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
 // sw.js - Service Worker Padronizado e Eficaz
-
 self.addEventListener('install', event => {
-  console.log('Service Worker: Instalando...');
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  console.log('Service Worker: Ativando...');
   return self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  // Apenas responde com a requisição da rede. Essencial para o PWA funcionar.
   event.respondWith(fetch(event.request));
 });
